89d677e58ea6f9839011d4d2c4be5f1604731673
LIVI-edit <livieditor@gmail.com>
2026-02-23 15:31:53 +0200

    Update script.js

--- api/script.js
+++ api/script.js
@@ -76,7 +76,7 @@ if (input.length < 2) {
       body: JSON.stringify({
         model: "gpt-4.1-mini",
         input,
-        max_output_tokens: 350
+        max_output_tokens: 900
       }),
     });
 
